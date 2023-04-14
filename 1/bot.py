9899150c6418fd6b8ee70b95b8777955d669a179
Pegoku <pegoku08@gmail.com>
2023-04-15 01:20:17 +0200

    add startup message

--- 1/bot.py
+++ 1/bot.py
@@ -8,7 +8,10 @@ wait_time = <time> # seconds
 channel_id = <ID> # channel id
 
 client = discord.Client(intents=intents)
-
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+    
 @client.event
 async def on_message(message):
     if message.channel.id == channel_id:
